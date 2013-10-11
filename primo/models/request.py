54cd06e5679af54e61d4acb31b38c5f815d68a6d
Martin Filliau <martin@filliau.com>
2013-10-11 15:03:23 +0100

    BaseRequest refactoring

--- primo/models/request.py
+++ primo/models/request.py
@@ -1,38 +1,36 @@
 from lxml import etree
 
-DEFAULT_NS = "http://www.exlibris.com/primo/xsd/wsRequest"
-NS = "{%s}" % DEFAULT_NS
-UIC_NS = "http://www.exlibris.com/primo/xsd/primoview/uicomponents"
-UIC = "{%s}" % UIC_NS
-PRIMO_NS = "http://www.exlibris.com/primo/xsd/search/request"
-PRIMO = "{%s}" % PRIMO_NS
+# namespaces
+DEFAULT_NS_URI = "http://www.exlibris.com/primo/xsd/wsRequest"
+DEFAULT_NS = "{%s}" % DEFAULT_NS_URI
+UIC_NS_URI = "http://www.exlibris.com/primo/xsd/primoview/uicomponents"
+UIC_NS = "{%s}" % UIC_NS_URI
+PRIMO_NS_URI = "http://www.exlibris.com/primo/xsd/search/request"
+PRIMO_NS = "{%s}" % PRIMO_NS_URI
 
-NSMAP = {None : DEFAULT_NS, 'uic': UIC_NS}
-PRIMONSMAP = { None: PRIMO_NS }
+REQUEST_NSMAP = {None: DEFAULT_NS_URI, 'uic': UIC_NS_URI}
+PRIMO_NSMAP = {None: PRIMO_NS_URI}
 
 
 class BaseRequest(object):
 
-    def __init__(self, institution, group, on_campus, ip):
-        pass
-
-
-class SearchBriefRequest(object):
-
-    def __init__(self, institution, group, on_campus, ip, query_terms_bool_operator="AND"):
-        self.request_root = etree.Element('searchRequest', nsmap=NSMAP)
-
+    def __init__(self, request, institution, group, on_campus, ip):
+        self.request_root = etree.Element(request, nsmap=REQUEST_NSMAP)
         self.request_root.append(self._add_element('institution', institution))
         self.request_root.append(self._add_element('group', group))
         self.request_root.append(self._add_element('onCampus', on_campus))
         self.request_root.append(self._add_element('ip', ip))
 
-        self.psr = etree.Element('PrimoSearchRequest', nsmap=PRIMONSMAP)
 
+class SearchBriefRequest(BaseRequest):
+
+    def __init__(self, institution, group, on_campus, ip, query_terms_bool_operator="AND"):
+        super(SearchBriefRequest, self).__init__('searchRequest', institution, group, on_campus, ip)
+        self.psr = etree.Element('PrimoSearchRequest', nsmap=PRIMO_NSMAP)
         self.query_terms = etree.Element('QueryTerms')
         self.query_terms.append(self._add_element('BoolOpeator', query_terms_bool_operator))
 
-    def append_psr_parameters(self):
+    def _append_psr_parameters(self):
         self.psr.append(self._add_element('StartIndex', "1"))
         self.psr.append(self._add_element('BulkSize', "20"))
         self.psr.append(self._add_element('DidUMeanEnabled', "false"))
@@ -49,7 +47,7 @@ class SearchBriefRequest(object):
         self.psr.append(self._add_element('DisplayFields', "creator"))
 
         locations = etree.Element('Locations')
-        location = etree.Element(UIC + "Location", type="local", value="")
+        location = etree.Element(UIC_NS + "Location", type="local", value="")
         locations.append(location)
         self.psr.append(locations)
 
@@ -67,7 +65,7 @@ class SearchBriefRequest(object):
 
     def get_request_as_string(self):
         self.psr.append(self.query_terms)
-        self.append_psr_parameters()
+        self._append_psr_parameters()
         self.request_root.append(self.psr)
         return etree.tostring(self.request_root, pretty_print=True)
 
